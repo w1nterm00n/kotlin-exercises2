c19b0a537c491bedbf227ac2693054ff0a151b7c
w1nterm00n <kordesovad@gmail.com>
2023-09-29 20:14:34 +0300

    добавила разшифровывание

--- 3.kt
+++ 3.kt
@@ -25,54 +25,110 @@ fun main() {
         }
     }
 
-    println("введите исходный текст")
-    var text = readln().toUpperCase()
-    var textDefaultCode = mutableListOf<Int>()
-    //шифрую текст первый раз
-    for (i in 0..(text.length-1)){
-        for (j in 0..(alphabet.size-1)){
-            if (alphabet[j] == text[i]) {
-                textDefaultCode.add(j)
-            }
-            if (text[i] == ' '){
-                textDefaultCode.add(34)
-            }
-        }
-    }
-    
-    //сдвигаю индексы и записываю новые значения в textChangedCode
-    var textChangedCode = mutableListOf<Int>()
-    var keyWordIndex = 0
-    for (i in 0..(textDefaultCode.size-1)){
-        if (textDefaultCode[i] != 34) {
-            if (keyWordIndex == keyWordCode.size) {
-                keyWordIndex = 0
-            }
-            var num = textDefaultCode[i] + keyWordCode[keyWordIndex]
-            if (num > 33){     //чтобы он был типа закольцованный
-                num = num - 33
-            }
-            textChangedCode.add(num)
-            keyWordIndex++
-        } else {
-            textChangedCode.add(34)
-        }
-    }
+    println("введите 1 чтобы зашифровать, или 2 чтобы расшифровать")
+    var mode = readLine()?.toInt() ?: 0
+    if (mode == 1) {                        //ЗАШИФРОВЫВАНИЕ
 
-    //расшифровываю в буквы
-
-    for (i in 0..(textChangedCode.size-1)){
-        if (textChangedCode[i] == 34){
-            print (" ")
-            continue
+        println("введите исходный текст")
+        var text = readln().toUpperCase()
+        var textDefaultCode = mutableListOf<Int>()
+        //шифрую текст первый раз
+        for (i in 0..(text.length-1)){
+            for (j in 0..(alphabet.size-1)){
+                if (alphabet[j] == text[i]) {
+                    textDefaultCode.add(j)
+                }
+                if (text[i] == ' '){
+                    textDefaultCode.add(34)     //34 значит пробел
+                }
+            }
         }
         
-        for (j in 0..(alphabet.size-1)){
-            if (textChangedCode[i] == j) {
-                print (alphabet[j])
+        //сдвигаю индексы и записываю новые значения в textChangedCode
+        var textChangedCode = mutableListOf<Int>()
+        var keyWordIndex = 0
+        for (i in 0..(textDefaultCode.size-1)){
+            if (textDefaultCode[i] != 34) {
+                if (keyWordIndex == keyWordCode.size) {
+                    keyWordIndex = 0
+                }
+                var num = textDefaultCode[i] + keyWordCode[keyWordIndex]
+                if (num > 33){     //чтобы он был типа закольцованный
+                    num = num - 33
+                }
+                textChangedCode.add(num)
+                keyWordIndex++
+            } else {
+                textChangedCode.add(34)
             }
         }
-    }    
+    
+        //расшифровываю в буквы
+        for (i in 0..(textChangedCode.size-1)){
+            if (textChangedCode[i] == 34){
+                print (" ")
+                continue
+            }
+            
+            for (j in 0..(alphabet.size-1)){
+                if (textChangedCode[i] == j) {
+                    print (alphabet[j])
+                }
+            }
+        }    
+    } else if (mode == 2) {                     //РАЗШИФРОВЫВАНИЕ
+        println("введите зашифрованный текст: ")
+        var text = readln().toUpperCase()
+        var textDefaultCode = mutableListOf<Int>()
+        //перевожу шифр в цифры
+        for (i in 0..(text.length-1)){
+            for (j in 0..(alphabet.size-1)){
+                if (alphabet[j] == text[i]) {
+                    textDefaultCode.add(j)
+                }
+                if (text[i] == ' '){
+                    textDefaultCode.add(34)
+                }
+            }
+        }
+
+        //сдвигаю индексы и записываю новые значения в textChangedCode
+        var textChangedCode = mutableListOf<Int>()
+        var keyWordIndex = 0
+        for (i in 0..(textDefaultCode.size-1)){
+            if (textDefaultCode[i] != 34) {
+                if (keyWordIndex == keyWordCode.size) {
+                    keyWordIndex = 0
+                }
+                var num = textDefaultCode[i] - keyWordCode[keyWordIndex]
+                if (num < 0){     //чтобы он был типа закольцованный
+                    num = num + 33
+                }
+                textChangedCode.add(num)
+                keyWordIndex++
+            } else {
+                textChangedCode.add(34)
+            }
+        }
+
+        //расшифровываю в буквы
+        for (i in 0..(textChangedCode.size-1)){
+            if (textChangedCode[i] == 34){
+                print (" ")
+                continue
+            }
+            
+            for (j in 0..(alphabet.size-1)){
+                if (textChangedCode[i] == j) {
+                    print (alphabet[j])
+                }
+            }
+        } 
+    } else {
+        println("ошибка")
+    }
+
+    println()
 }
 
 //сделать не чувствительным к регистру +
